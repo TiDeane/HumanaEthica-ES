ef10959f2b800790135bef60359f7ea85e359011
HenriqueProj <henrique.caroco3@gmail.com>
2024-02-28 12:59:16 +0000

    feat: Implemented ParticipationDto and ParticipationRepository

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/dto/ParticipationDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/dto/ParticipationDto.java
@@ -1,5 +1,79 @@
 package pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.dto;
 
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.dto.ActivityDto;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.domain.Participation;
+
 public class ParticipationDto {
+    private Integer id;
+    private Integer rating;
+    private String acceptanceDate;
+
+    private ActivityDto activity;
+    private Volunteer volunteer;
+
+    public ParticipationDto(){}
+
+    public ParticipationDto(Participation participation, boolean deepCopyParticipation){
+        setId(participation.getId());
+        setRating(participation.getRating());
+        setAcceptanceDate(participation.getAcceptanceDate());
+
+        if (deepCopyActivity && (participation.getActivity() != null)) {
+            setActivity(new ActivityDto(participation.getActivity(), false));
+        }
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getAcceptanceDate() {
+        return acceptanceDate;
+    }
+
+    public void setAcceptanceDate(String acceptanceDate) {
+        this.acceptanceDate = acceptanceDate;
+    }
+
+    public ActivityDto getActivity() {
+        return activity;
+    }
+
+    public void setActivity(ActivityDto activity) {
+        this.activity = activity;
+    }
+
+    public Volunteer getVolunteer(){
+        return volunteer;
+    }
+
+    public void setVolunteer(Volunteer volunteer){
+        this.volunteer = volunteer;
+    }
+
+    @Override
+    public String toString() {
+        return "ParticipationDto{" +
+                "id=" + id +
+                ", rating='" + rating + '\'' +
+                ", acceptanceDate='" + acceptanceDate + '\'' +
+                ", activity=" + activity +
+                ", user=" + volunteer +
+                '}';
+    }
 
 }
