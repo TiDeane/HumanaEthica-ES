8e09fa2b8d1b13c69affb06bf7090acc3297ffca
Pedro Gomes <pedroafonsogomes3@tecnico.ulisboa.pt>
2024-03-03 18:43:41 +0000

    feat(assessment): Initialize Repository. Implemented a method to fetch assessments by institution ID.

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/repository/AssessmentRepository.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/repository/AssessmentRepository.java
@@ -1,5 +1,16 @@
 package pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.repository;
 
-public interface AssessmentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.domain.Assessment;
 
+import java.util.List;
+
+@Repository
+@Transactional
+public interface AssessmentRepository extends JpaRepository<Assessment, Integer> {
+    @Query("SELECT a FROM Assessment a WHERE a.institution.id = :institutionId")
+    List<Assessment> getAssessmentByInstitutionId(Integer institutionId);
 }
