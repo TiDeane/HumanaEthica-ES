e15e0b14b3126a5732f953610b89a8ea40056a08
Tiago Deane <tiagodeane@tecnico.ulisboa.pt>
2024-03-07 17:25:50 +0000

    fix: renamed CreateGetEnrollmentsByActivirtyService to GetEnrollmentsByActivityServiceTest; added an additional enrollment to the test

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/service/GetEnrollmentsByActivityServiceTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/service/GetEnrollmentsByActivityServiceTest.groovy
@@ -4,23 +4,15 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.TestConfiguration
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.BeanConfiguration
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.SpockTest
-import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.dto.ActivityDto
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.auth.domain.AuthUser
-import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.ErrorMessage
-import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.HEException
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.theme.domain.Theme
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity
-import pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.dto.EnrollmentDto
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.User
-import pt.ulisboa.tecnico.socialsoftware.humanaethica.utils.DateHandler
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.domain.Enrollment
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer
-import spock.lang.Unroll
-
-import javax.swing.plaf.nimbus.State
 
 @DataJpaTest
-class CreateGetEnrollmentsByActivityService extends SpockTest {
+class GetEnrollmentsByActivityServiceTest extends SpockTest {
     // Declare activity at the class level
     Activity activity
 
@@ -29,23 +21,29 @@ class CreateGetEnrollmentsByActivityService extends SpockTest {
         given: "activity info"
         def activityDto = createActivityDto(ACTIVITY_NAME_1, ACTIVITY_REGION_1, 1, ACTIVITY_DESCRIPTION_1,
                 IN_ONE_DAY, IN_TWO_DAYS, IN_THREE_DAYS, null)
-        and: "a theme"
+        and: "themes"
         def themes = new ArrayList<>()
         themes.add(createTheme(THEME_NAME_1, Theme.State.APPROVED, null))
         and: "an activity"
         activity = new Activity(activityDto, institution, themes)
         activityRepository.save(activity)
         and: "a volunteer"
-        Volunteer volunteer = new Volunteer(USER_1_NAME,USER_1_USERNAME, USER_1_EMAIL, AuthUser.Type.DEMO, User.State.SUBMITTED )
+        Volunteer volunteer = new Volunteer(USER_1_NAME,USER_1_USERNAME, USER_1_EMAIL, AuthUser.Type.DEMO, User.State.SUBMITTED)
         userRepository.save(volunteer)
-        and: "enrollment info"
+        and: "another volunteer"
+        Volunteer volunteer2 = new Volunteer(USER_2_NAME,USER_2_USERNAME, USER_2_EMAIL, AuthUser.Type.DEMO, User.State.SUBMITTED)
+        userRepository.save(volunteer2)
+        and: "an enrollment"
         def enrollmentDto = createEnrollmentDto(MOTIVATION_1)
         def enrollment = new Enrollment(enrollmentDto, activity, volunteer)
-        and: "add enrollment"
         enrollmentRepository.save(enrollment)
+        and: "another enrollment"
+        def enrollmentDto2 = createEnrollmentDto(MOTIVATION_2)
+        def enrollment2 = new Enrollment(enrollmentDto2, activity, volunteer2)
+        enrollmentRepository.save(enrollment2)
     }
 
-    def "get enrollment by activity" () {
+    def "get two enrollments" () {
         given: "an activity id"
         def activityId = activity.getId()
 
@@ -53,9 +51,9 @@ class CreateGetEnrollmentsByActivityService extends SpockTest {
         def result = enrollmentService.getEnrollmentsByActivity(activityId)
 
         then:
-        "no enrollment is stored in the database of this activity"
-        result.size() == 1
+        result.size() == 2
         result.get(0).motivation == MOTIVATION_1
+        result.get(1).motivation == MOTIVATION_2
     }
 
     @TestConfiguration
