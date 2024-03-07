e78a5ce52c88fe07632121b4c1d4b95956a8d182
Luís Calado <luis.maria.calado@tecnico.ulisboa.pt>
2024-03-07 22:22:51 +0000

    feat(Participation): Implemented Participation WebServices Get and Post

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/ParticipationController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/ParticipationController.java
@@ -1,5 +1,37 @@
 package pt.ulisboa.tecnico.socialsoftware.humanaethica.participation;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.ParticipationService;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.dto.ParticipationDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.auth.domain.AuthUser;
+
+import java.security.Principal;
+import java.util.List;
+
+@RestController
+@RequestMapping("/participations")
 public class ParticipationController {
+    @Autowired
+    private ParticipationService participationService;
+
+    private static final Logger logger = LoggerFactory.getLogger(ParticipationController.class);
+
+    @GetMapping("/{activityId}/participants")
+    @PreAuthorize("(hasRole('ROLE_MEMBER')) and hasPermission(#activityId, 'ACTIVITY.MEMBER')")
+    public List<ParticipationDto> getActivityParticipations(@PathVariable Integer activityId) {
+        return participationService.getParticipationsByActivity(activityId);
+    }
+
+    @PostMapping("/{activityId}")
+    @PreAuthorize("(hasRole('ROLE_MEMBER'))")
+    public ParticipationDto createParticipation(@PathVariable Integer activityId, @Valid @RequestBody ParticipationDto participationDto) {
+        return participationService.createParticipation(activityId, participationDto);
+    }
 
 }
