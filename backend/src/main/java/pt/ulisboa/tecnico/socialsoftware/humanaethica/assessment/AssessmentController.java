65ac940e4ebdf4e639205fc2134b254b340e0f29
Artur Krystopchuk <arturkrystopchuk@tecnico.ulisboa.pt>
2024-03-08 06:50:09 +0000

    feat(Assessment): implmented get and create in AssessmentController

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/AssessmentController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/AssessmentController.java
@@ -1,5 +1,36 @@
 package pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment;
 
-public class AssessmentController {
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.dto.ActivityDto;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.dto.AssessmentDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.auth.domain.AuthUser;
 
+import java.security.Principal;
+import java.util.List;
+
+@RestController
+@RequestMapping("/assessments")
+public class AssessmentController {
+    @Autowired
+    private AssessmentService assessmentService;
+
+    private static final Logger logger = LoggerFactory.getLogger(AssessmentController.class);
+
+    @GetMapping()
+    public List<AssessmentDto> getInstitutionAssessments(@PathVariable Integer institutionId) {
+        return assessmentService.getAssessmentsByInstitution(institutionId);
+    }
+
+    @PutMapping("/{institutionId}")
+    @PreAuthorize("(hasRole('ROLE_VOLUNTEER'))")
+    public AssessmentDto createAssessment(Principal principal, @PathVariable Integer institutionId, @Valid @RequestBody AssessmentDto assessmentDto) {
+        int userId = ((AuthUser) ((Authentication) principal).getPrincipal()).getUser().getId();
+        return assessmentService.createAssessment(userId, institutionId, assessmentDto);
+    }
 }
