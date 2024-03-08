31997ea6a4f12af1c8725b4f57f298ce71cfcace
HenriqueProj <henrique.caroco3@gmail.com>
2024-03-08 11:41:25 +0000

    test(Participation): Implemented CreateParticipation WebServiceIT test

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/webservice/CreateParticipationWebServiceIT.groovy
@@ -0,0 +1,182 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.webservice
+
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.SpockTest
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.dto.ActivityDto
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.auth.domain.AuthUser
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.institution.domain.Institution
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.domain.Participation
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.dto.ParticipationDto
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.theme.domain.Theme
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.theme.dto.ThemeDto
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.User
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CreateParticipationWebServiceIT extends SpockTest {
+    @LocalServerPort
+    private int port
+
+    def activityDto
+    def activity
+    def activityId
+    def volunteer
+    def participationDto
+
+    def setup() {
+        deleteAll()
+
+        webClient = WebClient.create("http://localhost:" + port)
+        headers = new HttpHeaders()
+        headers.setContentType(MediaType.APPLICATION_JSON)
+
+        def user = demoMemberLogin()
+
+        def theme = createTheme(THEME_NAME_1, Theme.State.APPROVED,null)
+        def themesDto = new ArrayList<>()
+        themesDto.add(new ThemeDto(theme,false, false, false))
+
+        activityDto = createActivityDto(ACTIVITY_NAME_1,ACTIVITY_REGION_1,2,ACTIVITY_DESCRIPTION_1,
+                ONE_DAY_AGO,IN_TWO_DAYS,IN_THREE_DAYS,themesDto)
+
+        def activity = activityService.registerActivity(user.id, activityDto)
+        activityId = activity.id
+
+        volunteer = new Volunteer(USER_1_NAME, USER_1_USERNAME, USER_1_EMAIL, AuthUser.Type.NORMAL, User.State.ACTIVE)
+        userRepository.save(volunteer)
+
+        participationDto = createParticipationDto(RATING_1, volunteer.id)
+
+        //participationService.createParticipation(activityId, participationDto)
+    }
+
+    def "login as member, and create a participation"() {
+        given:
+        demoMemberLogin()
+
+        when:
+        def response = webClient.post()
+                .uri('/participations/' + activityId)
+                .headers(httpHeaders -> httpHeaders.putAll(headers))
+                .bodyValue(participationDto)
+                .retrieve()
+                .bodyToMono(ParticipationDto.class)
+                .block()
+
+        then: "check response data"
+        response.rating == RATING_1
+        response.volunteerId == volunteer.id
+        response.acceptanceDate != null
+
+        and: 'check database data'
+        participationRepository.count() == 1
+        def participation = participationRepository.findAll().get(0)
+        participation.getRating() == RATING_1
+        participation.getVolunteer().getId() == volunteer.id
+        participation.getAcceptanceDate() != null
+
+        cleanup:
+        deleteAll()
+    }
+
+    def "login as member, and create a participation with error"() {
+        given: 'a member'
+        demoMemberLogin()
+        and: 'a null rating'
+        participationDto.rating = null
+
+        when: 'the member registers the activity'
+        webClient.post()
+                .uri('/participations/' + activityId)
+                .headers(httpHeaders -> httpHeaders.putAll(headers))
+                .bodyValue(participationDto)
+                .retrieve()
+                .bodyToMono(ParticipationDto.class)
+                .block()
+
+        then: "check response status"
+        def error = thrown(WebClientResponseException)
+        error.statusCode == HttpStatus.BAD_REQUEST
+        participationRepository.count() == 0
+
+        cleanup:
+        deleteAll()
+    }
+
+    def "login as volunteer, and try to create a participation"() {
+        given: 'a volunteer'
+        demoVolunteerLogin()
+
+        when: 'the volunteer creates the participation'
+        webClient.post()
+                .uri('/participations/' + activityId)
+                .headers(httpHeaders -> httpHeaders.putAll(headers))
+                .bodyValue(participationDto)
+                .retrieve()
+                .bodyToMono(ParticipationDto.class)
+                .block()
+
+        then: "an error is returned"
+        def error = thrown(WebClientResponseException)
+        error.statusCode == HttpStatus.FORBIDDEN
+        participationRepository.count() == 0
+
+        cleanup:
+        deleteAll()
+    }
+
+    def "login as admin, and and try to create a participation"() {
+        given: 'a demo'
+        demoAdminLogin()
+
+        when: 'the admin registers the activity'
+        webClient.post()
+                .uri('/participations/' + activityId)
+                .headers(httpHeaders -> httpHeaders.putAll(headers))
+                .bodyValue(participationDto)
+                .retrieve()
+                .bodyToMono(ParticipationDto.class)
+                .block()
+
+        then: "an error is returned"
+        def error = thrown(WebClientResponseException)
+        error.statusCode == HttpStatus.FORBIDDEN
+        participationRepository.count() == 0
+
+        cleanup:
+        deleteAll()
+    }
+
+    def "login as member of another institution and cannot create participation"() {
+        given:
+        def otherInstitution = new Institution(INSTITUTION_1_NAME, INSTITUTION_1_EMAIL, INSTITUTION_1_NIF)
+        institutionRepository.save(otherInstitution)
+        def otherMember = createMember(USER_3_NAME,USER_3_USERNAME,USER_2_PASSWORD,USER_3_EMAIL, AuthUser.Type.NORMAL, otherInstitution, User.State.APPROVED)
+        normalUserLogin(USER_3_USERNAME, USER_2_PASSWORD)
+
+        when:
+        webClient.post()
+                .uri('/participations/' + activityId)
+                .headers(httpHeaders -> httpHeaders.putAll(headers))
+                .bodyValue(participationDto)
+                .retrieve()
+                .bodyToMono(ParticipationDto.class)
+                .block()
+
+        then: "an error is returned"
+        def error = thrown(WebClientResponseException)
+        error.statusCode == HttpStatus.FORBIDDEN
+        participationRepository.count() == 0
+
+        cleanup:
+        deleteAll()
+    }
+}
+
