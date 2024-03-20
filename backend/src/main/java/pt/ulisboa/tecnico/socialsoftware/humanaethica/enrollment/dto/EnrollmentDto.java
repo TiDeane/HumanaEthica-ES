cb2a2e1934d132efc2a8cb13135a9fc466616e22
Luís Calado <luis.maria.calado@tecnico.ulisboa.pt>
2024-03-20 19:12:22 +0000

    feat(Participation): Implemented Select Participant button in InstitutionActivityEnrollmentsView; Also fixed attribute participating from EnrollmentDto since wasn't appearing correctly in the frontend Refs #45 , #44

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/dto/EnrollmentDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/dto/EnrollmentDto.java
@@ -23,7 +23,7 @@ public class EnrollmentDto {
         this.enrollmentDateTime = DateHandler.toISOString(enrollment.getEnrollmentDateTime());
         this.activityId = enrollment.getActivity().getId();
         this.volunteerName = enrollment.getVolunteer().getName();
-        this.participating = this.isParticipating(enrollment);
+        this.participating = isParticipating(enrollment);
     }
 
     public Integer getId() {
@@ -64,6 +64,14 @@ public class EnrollmentDto {
 
     public void setVolunteerName(String volunteerName) {
         this.volunteerName = volunteerName;
+    }
+
+    public boolean getParticipating() {
+        return participating;
+    }
+
+    public void setParticipating(boolean participating) {
+        this.participating = participating;
     }
 
     public boolean isParticipating(Enrollment enrollment) {
