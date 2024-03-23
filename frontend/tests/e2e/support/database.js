cd6414e77287a0b3d0c20ee852a417e32d03f8dc
Luís Calado <luis.maria.calado@tecnico.ulisboa.pt>
2024-03-23 17:12:00 +0000

    test(Participation): Implemented data initialization for e2e test Select Participant Refs #53

--- frontend/tests/e2e/support/database.js
+++ frontend/tests/e2e/support/database.js
@@ -11,6 +11,7 @@ const USER_COLUMNS = "users (user_type, id, creation_date, name, role, state, in
 const AUTH_USERS_COLUMNS = "auth_users (auth_type, id, active, email, username, user_id)";
 const ACTIVITY_COLUMNS = "activity (id, application_deadline, creation_date, description, ending_date, name, participants_number_limit, region, starting_date, state, institution_id)";
 const ENROLLMENT_COLUMNS = "enrollment (id, enrollment_date_time, motivation, activity_id, volunteer_id)";
+const PARTICIPATION_COLUMNS = "participation (id, acceptance_date, rating, activity_id, volunteer_id)";
 
 const now = new Date();
 const tomorrow = new Date(now);
@@ -25,6 +26,14 @@ dayBeforeYesterday.setDate(now.getDate() - 2);
 Cypress.Commands.add('deleteAllButArs', () => {
   cy.task('queryDatabase', {
     query: "DELETE FROM ENROLLMENT",
+    credentials: credentials,
+  });
+  cy.task('queryDatabase', {
+    query: "DELETE FROM PARTICIPATION",
+    credentials: credentials,
+  });
+  cy.task('queryDatabase', {
+    query: "DELETE FROM ASSESSMENT",
     credentials: credentials,
   });
   cy.task('queryDatabase', {
@@ -63,10 +72,54 @@ Cypress.Commands.add('createDemoEntities', () => {
     credentials: credentials,
   })
   cy.task('queryDatabase',  {
+    query: "INSERT INTO " + USER_COLUMNS + generateUserTuple(4, "VOLUNTEER","DEMO-VOLUNTEER2", "VOLUNTEER", "NULL"),
+    credentials: credentials,
+  })
+  cy.task('queryDatabase',  {
+    query: "INSERT INTO " + USER_COLUMNS + generateUserTuple(5, "VOLUNTEER","DEMO-VOLUNTEER3", "VOLUNTEER", "NULL"),
+    credentials: credentials,
+  })
+  cy.task('queryDatabase',  {
     query: "INSERT INTO " + AUTH_USERS_COLUMNS + generateAuthUserTuple(3, "DEMO", "demo-volunteer", 3),
     credentials: credentials,
   })
+  cy.task('queryDatabase',  {
+    query: "INSERT INTO " + AUTH_USERS_COLUMNS + generateAuthUserTuple(4, "DEMO", "demo-volunteer-2", 4),
+    credentials: credentials,
+  })
+  cy.task('queryDatabase',  {
+    query: "INSERT INTO " + AUTH_USERS_COLUMNS + generateAuthUserTuple(5, "DEMO", "demo-volunteer-3", 5),
+    credentials: credentials,
+  })
 });
+
+Cypress.Commands.add(
+  'createActivity',
+  (id, application_deadline, creation_date, description, ending_date, name, participants_number_limit, region, starting_date, state, institution_id) => {
+  cy.task('queryDatabase', {
+    query: "INSERT INTO " + ACTIVITY_COLUMNS + generateActivityTuple(id, application_deadline, creation_date, description, ending_date, name, participants_number_limit, region, starting_date, state, institution_id),
+    credentials: credentials,
+  })
+})
+
+Cypress.Commands.add(
+  'createEnrollment',
+  (id, enrollment_date_time, motivation, activity_id, volunteer_id) => {
+  cy.task('queryDatabase', {
+    query: "INSERT INTO " + ENROLLMENT_COLUMNS + generateEnrollmentTuple(id, enrollment_date_time, motivation, activity_id, volunteer_id),
+    credentials: credentials,
+  })
+})
+
+Cypress.Commands.add(
+  'createParticipation',
+  (id, acceptance_date, rating, activity_id, volunteer_id) => {
+  cy.task('queryDatabase', {
+    query: "INSERT INTO " + PARTICIPATION_COLUMNS + generateParticipationTuple(id, acceptance_date, rating, activity_id, volunteer_id),
+    credentials: credentials,
+  })
+  }
+)
 
 function generateAuthUserTuple(id, authType, username, userId) {
   return "VALUES ('"
@@ -130,6 +183,15 @@ function generateEnrollmentTuple(id, enrollment_date_time, motivation, activity_
     + motivation + "', '"
     + activity_id + "', "
     + volunteer_id + ")";
+}
+
+function generateParticipationTuple(id, acceptance_date, rating, activity_id, volunteer_id) {
+  return "VALUES ('"
+    + id +  "', '"
+    + acceptance_date +  "', '"
+    + rating +  "', '"
+    + activity_id +  "', "
+    + volunteer_id +  ")";
 }
 
 Cypress.Commands.add(
