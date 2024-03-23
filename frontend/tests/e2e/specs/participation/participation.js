eb2f3b9e2f622a9de8bfd6c240a1a484b0920d07
Luís Calado <luis.maria.calado@tecnico.ulisboa.pt>
2024-03-23 18:02:41 +0000

    test(Participation): Verify activities and enrollments tables for select participant e2e test Refs #54

--- frontend/tests/e2e/specs/participation/participation.js
+++ frontend/tests/e2e/specs/participation/participation.js
@@ -54,7 +54,34 @@ describe('SelectParticipant', () => {
 
 
     it('Select Participant', () => { 
-      // TODO
+      
+      cy.demoMemberLogin();
 
+      cy.get('[data-cy="institution"]').click();
+      cy.intercept('GET', '/users/*/getInstitution').as('getInstitutions');
+      cy.intercept('GET', '/themes/availableThemes').as('availableTeams');
+
+      
+      cy.get('[data-cy="activities"]').click();
+      cy.wait('@getInstitutions');
+      cy.wait('@availableTeams');
+
+      cy.get('[data-cy="memberActivitiesTable"] tbody tr')
+      .should('have.length', 2);
+
+
+      cy.get('[data-cy="memberActivitiesTable"] tbody tr')
+      .eq(0).children().eq(3).should('contain', 1);
+
+      cy.intercept('GET', '/activities/*/enrollments').as('activityEnrollments');
+      
+      cy.get('[data-cy="showEnrollments"]').eq(0).click();
+      cy.wait('@activityEnrollments');
+
+      cy.get('[data-cy="activityEnrollmentsTable"] tbody tr').should('have.length', 2);
+
+      cy.get('[data-cy="activityEnrollmentsTable"] tbody tr')
+      .eq(0).children().eq(2).should('contain', false);
+    
     });
 });
