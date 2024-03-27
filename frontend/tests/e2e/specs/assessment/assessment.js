87b0cbac66c7f6db50c0d2e49f4ba9f2f12bab62
Pedro Gomes <pedroafonsogomes3@tecnico.ulisboa.pt>
2024-03-27 12:44:59 +0000

    feat(assessment): Implemented Wait on Remote Invocations  in E2E Assessment Test (#64)

--- frontend/tests/e2e/specs/assessment/assessment.js
+++ frontend/tests/e2e/specs/assessment/assessment.js
@@ -17,7 +17,9 @@ describe('Assessment', () => {
         cy.demoVolunteerLogin()
 
         // Go to activities view
+        cy.intercept('GET', '/activities').as('getActivities');
         cy.get('[data-cy="volunteerActivities"]').click();
+        cy.wait('@getActivities');
 
         // Verify number of activities
         cy.get('[data-cy="volunteerActivitiesTable"] tbody tr')
@@ -28,9 +30,12 @@ describe('Assessment', () => {
             .eq(0).children().eq(0).should('contain', NAME)
 
         // Create an assessment
+        cy.intercept('POST', '/institutions/*/assessments', (req) => {
+        }).as('register');
         cy.get('[data-cy="createAssessmentButton"]').first().click();
         cy.get('[data-cy="reviewInput"]').type(REVIEW);
         cy.get('[data-cy="createAssessment"]').click();
+        cy.wait('@register')
 
         cy.logout();
 
@@ -38,8 +43,12 @@ describe('Assessment', () => {
         cy.demoMemberLogin()
 
         // Go to assessments view
+        cy.intercept('GET', '/users/*/getInstitution').as('getInstitutions');
+        cy.intercept('GET', '/institutions/*/assessments').as('assessments')
         cy.get('[data-cy="institution"]').click();
         cy.get('[data-cy="assessments"]').click();
+        cy.wait('@getInstitutions');
+        cy.wait('@assessments');
 
         // Verify number of assessments
         cy.get('[data-cy="institutionAssessmentsTable"] tbody tr')
@@ -50,6 +59,5 @@ describe('Assessment', () => {
             .eq(0).children().eq(0).should('contain', REVIEW)
 
         cy.logout();
-
     });
 });
